b1afe02766d4224e08b5fbaf953f05442ae655ac
DTS4You <info@datentechnikschwarz.de>
2025-11-23 16:03:59 +0100

    Init

--- libs/module_xio.py
+++ libs/module_xio.py
@@ -4,38 +4,49 @@
 # #############################################################################
 
 from machine import Pin, PWM        # type: ignore
-#from utime import sleep             # type: ignore
+from utime import sleep             # type: ignore
 
 
 class XIO:
 
     def __init__(self, dir):
         self.dir = dir
-        self.io_0 = False
-        self.io_1 = False
-        self.io_2 = False
-        self.io_3 = False
-        self.pin_n = []
+        self.io = []
+        self.io.append(False)
+        self.io.append(False)
+        self.io.append(False)
+        self.io.append(False)
+        self.pin = []
         if dir:
             self.set_xio_out()
         else:
             self.set_xio_in()
 
     def set_xio_out(self):
-        self.pin_n.append(Pin(10, mode=Pin.OUT))
-        self.pin_n.append(Pin(11, mode=Pin.OUT))
-        self.pin_n.append(Pin(12, mode=Pin.OUT))
-        self.pin_n.append(Pin(13, mode=Pin.OUT))
+        self.pin.append(Pin(10, mode=Pin.OUT))
+        self.pin.append(Pin(11, mode=Pin.OUT))
+        self.pin.append(Pin(12, mode=Pin.OUT))
+        self.pin.append(Pin(13, mode=Pin.OUT))
 
     def set_xio_in(self):
-        self.pin_n[0] = Pin(10, mode=Pin.IN, pull=Pin.PULL_UP)
-        self.pin_n[1] = Pin(11, mode=Pin.IN, pull=Pin.PULL_UP)
-        self.pin_n[2] = Pin(12, mode=Pin.IN, pull=Pin.PULL_UP)
-        self.pin_n[3] = Pin(13, mode=Pin.IN, pull=Pin.PULL_UP)
+        self.pin.append(Pin(10, mode=Pin.IN, pull=Pin.PULL_UP))
+        self.pin.append(Pin(11, mode=Pin.IN, pull=Pin.PULL_UP))
+        self.pin.append(Pin(12, mode=Pin.IN, pull=Pin.PULL_UP))
+        self.pin.append(Pin(13, mode=Pin.IN, pull=Pin.PULL_UP))
+
+    def read_input(self):
+        pass
+
+    def write_output(self):
+        for i in range(4):
+            self.pin[i].value(self.io[i])
     
-    def set_out(self):
-        self.pin_n[0].value(1)
-        self.pin_n[1].value(0)
+    def set_bit(self, bit, value=True):
+        self.io[bit] = value
+        return self.io[bit]
+    
+    def get_bit(self, bit):
+        return self.io[bit]
     
 # -----------------------------------------------------------------------------
 def main():
@@ -47,7 +58,16 @@ def main():
 
         xio = XIO(1)
 
-        xio.set_out()
+        xio.write_output()
+
+        sleep(1)
+        
+        xio.set_bit(0, True)
+        xio.set_bit(1, False)
+        xio.set_bit(2, True)
+        xio.set_bit(3, False)
+
+        xio.write_output()
 
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
